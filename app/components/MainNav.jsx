1e4910cb6f888fc42b45de894c203938587d9090
Trevor_143 <trevorarapu2@gmail.com>
2023-08-30 15:59:46 -0400

    first commit

---
+++ app/components/MainNav.jsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import Logo from './dojo-logo.png'
+
+export default function NavBar () {
+    return (
+        <nav>
+            <Image
+            src={Logo}
+            alt='desk logo'
+            width={70}
+            quality={100}
+            placeholder='blur'
+            />
+          <h2>The navigation</h2>
+          <Link href="/" >Home</Link>
+          <Link href="/tickets">Tickets</Link>
+        </nav>
+    )
+}
